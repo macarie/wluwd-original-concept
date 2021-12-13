760207dad8ff2c12973bd6a52fea6f7538ff8684
Raul Macarie <raul@macarie.me>
2021-12-13 22:54:11 +0100

    feat(assert): convert `equalTo` to be an `EnhancedTester`

--- packages/assert/source/testers.ts
+++ packages/assert/source/testers.ts
@@ -1,1 +1,10 @@
-export { dequal as equalTo } from 'dequal'
+import { dequal } from 'dequal'
+
+import { createTester } from './utils/create-tester.js'
+
+const dequalCopy = dequal.bind(this)
+
+export const equalTo = createTester(dequalCopy, ({ args: [input, expected], createDiff, inspect }) => ({
+  message: 'It seems that `input` and `expected` are not deeply equal:',
+  content: createDiff(inspect(input), inspect(expected)),
+}))
