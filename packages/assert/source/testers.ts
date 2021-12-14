e99058aaf89fc3e8f0bee1be043119e0c4a240ab
Raul Macarie <raul@macarie.me>
2021-12-15 00:01:16 +0100

    feat(assert): use the `equalTo` error message from `/common`

--- packages/assert/source/testers.ts
+++ packages/assert/source/testers.ts
@@ -1,10 +1,13 @@
 import { dequal } from 'dequal'
+import { messageSymbols } from '@wluwd/common/message-symbols'
 
 import { createTester } from './utils/create-tester.js'
 
 const dequalCopy = dequal.bind(this)
 
-export const equalTo = createTester(dequalCopy, ({ args: [input, expected], createDiff, inspect }) => ({
-  message: 'It seems that `input` and `expected` are not deeply equal:',
+const getType = (negated: boolean): 'negative' | 'affirmative' => (negated ? 'negative' : 'affirmative')
+
+export const equalTo = createTester(dequalCopy, ({ args: [input, expected], createDiff, inspect, negated }) => ({
+  message: messageSymbols[getType(negated)].equalTo,
   content: createDiff(inspect(input), inspect(expected)),
 }))
